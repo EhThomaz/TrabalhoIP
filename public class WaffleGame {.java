e9920f937c6a4d632f921202bceef63139be0590
Eduardo Thomaz <eh.thomaz@hotmail.com>
2022-10-21 03:43:47 +0100

    dasasa

--- public class WaffleGame {.java
+++ public class WaffleGame {.java
@@ -3,7 +3,7 @@ public class WaffleGame {
     public static void main(String[] args) {
       //codigo que implementa determinada tarefa
       int num
-      num = 244
+      num = 24423
     }
      //definição de funções auxiliarees
 }

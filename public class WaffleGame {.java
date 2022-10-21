46d44aa00b2c536cbacbf943006521f4e9955364
Eduardo Thomaz <eh.thomaz@hotmail.com>
2022-10-21 03:36:47 +0100

    Oi

---
+++ public class WaffleGame {.java
@@ -0,0 +1,9 @@
+public class WaffleGame {
+
+    public static void main(String[] args) {
+      //codigo que implementa determinada tarefa
+      int num
+      num =
+    }
+     //definição de funções auxiliares
+}
